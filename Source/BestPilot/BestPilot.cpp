b8006d05e252c95a12a36fa40647d54c1063da15
heesoo1114 <improveme7@gmail.com>
2024-05-20 13:57:13 +0900

    Initial commit

---
+++ Source/BestPilot/BestPilot.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BestPilot.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BestPilot, "BestPilot" );
